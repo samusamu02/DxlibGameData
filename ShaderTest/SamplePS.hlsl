6df6ee2e83bdda097a594e77a0fb0e75f09516bd
samusamu02 <minecraftdaisuki43@gmail.com>
2022-05-20 13:47:10 +0900

    画像サイズの変更

--- ShaderTest/SamplePS.hlsl
+++ ShaderTest/SamplePS.hlsl
@@ -13,28 +13,48 @@ struct PS_OUTPUT
 };
 
 // 定数バッファ
-float threshold : register(c0);
+cbuffer buff : register(b0) {
+	float threshold;
+	float angle;
+}
 
 SamplerState sam;		// サンプラ
 Texture2D tex : register(t0);			// テクスチャ
 Texture2D ptn: register (t1);			// テクスチャ
+Texture2D norm: register (t2);			// 法線マップ
 
 // 1ピクセル塗りつぶし際の呼ばれる
 PS_OUTPUT main(PS_INPUT input)
 {
 	PS_OUTPUT output;
-	float discVal = ptn.Sample(sam, input.uv).r;
-	if (discVal > threshold)
+	float discVal = ptn.Sample(sam, input.uv).r;		// rの値を抽出
+	float yedge = pow(1.0 - saturate(abs(threshold-input.uv.y)),3);
+	if (input.uv.y + (yedge * discVal) < threshold)
 	{
 		discard;
 	}
 
-	float4 col = tex.Sample(sam, input.uv);//float4(input.uv,0.5, 1);		// 赤を返す
+	float3 light = float3(cos(angle),sin(angle), 1);
+	light = normalize(light);
+	float3 normal = norm.Sample(sam, input.uv).rgb;
+	normal = (normal * 2) - 1;
+	normal = normalize(normal);
+	normal.z = -normal.z;
+	float bright = saturate(saturate(dot(-light,normal)) + 0.25);
+
+
+	float4 col = tex.Sample(sam, input.uv+float2(sin(angle)*0.0f,0));		//float4(input.uv,0.5, 1);		// 赤を返す
 	output.color = col;
 	if (col.a == 0.0)
 	{
 		discard;		// なかったことにする
 	}
-	output.color.rgb = 1.0 - output.color.rgb;
+
+	float edge = 1.0 - saturate(abs(threshold - discVal));
+	edge = pow(1.0 - saturate(abs(threshold - input.uv.y)), 3);
+	edge = pow(edge, 20);
+
+	// output.color.rgb = 1.0 - output.color.rgb;		// 色を反転
+	output.color.rgb = float3 (0, 1, 0) * edge + (output.color.rgb * bright) * (1.0 - edge); // float3(0,pow(edge,20), 0);
 	return output;
 }
--- ShaderTest/main.cpp
+++ ShaderTest/main.cpp
@@ -1,7 +1,15 @@
 #include <DxLib.h>
 #include <array>
-
-void ShadeGraph(int x, int y,int imageHandle,int secondHandle,int psHandle)
+#include<math.h>
+/// <summary>
+/// シェーダー用のDrawGraph
+/// </summary>
+/// <param name="x">x座標</param>
+/// <param name="y">y座標</param>
+/// <param name="imageHandle">一つ目の画像ハンドルID</param>
+/// <param name="secondHandle">二つ目の画像ハンドルID</param>
+/// <param name="psHandle">ピクセルシェーダのハンドル</param>
+void ShadeDrawGraph(int x, int y,int imageHandle,int secondHandle,int tharerdimag,int psHandle)
 {
 	int width, hight;
 	GetGraphSize(imageHandle, &width, &hight);
@@ -49,6 +57,7 @@ void ShadeGraph(int x, int y,int imageHandle,int secondHandle,int psHandle)
 	SetUsePixelShader(psHandle);
 	SetUseTextureToShader(0, imageHandle);
 	SetUseTextureToShader(1, secondHandle);
+	SetUseTextureToShader(2, tharerdimag);
 	DrawPrimitive2DToShader(vertx.data(),vertx.size(),DX_PRIMTYPE_TRIANGLESTRIP);
 }
 
@@ -59,39 +68,40 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	int image = LoadGraph(L"image/cookie.png");
+	int normImg = LoadGraph(L"image/cookie_n.png");
 	int bg = LoadGraph(L"image/bg.jpg");
 	int pt = LoadGraph(L"image/ptn.png");
 	int ps = LoadPixelShader(L"SamplePS.pso");
 
-	// ①定数バッファの確保
-	int cBuff = DxLib::CreateShaderConstantBuffer(sizeof(float) * 4);
+
+	// ①定数バッファの確保(VRAM上のメモリを確保）
+	int cBuff = DxLib::CreateShaderConstantBuffer(sizeof(float) * 4);		// ハンドルIDみたいなやつ
+	// グラボのメモリはいじれない。
+	// RAM上のメモリでシミュレーションしている。
 	float* threshold = static_cast<float*>(DxLib::GetBufferShaderConstantBuffer(cBuff));
 	float data = 0.0f;
+	float angle = 0.0f;
 	char keystate[256];
 	while (ProcessMessage() != -1)
 	{
+		angle += 0.001f;
 		GetHitKeyStateAll(keystate);
 		if (keystate[KEY_INPUT_UP])
 		{
-			if (data >= 0.00f)
-			{
-				data -= 0.01f;
-			}
+			data -= 0.01f;
 		}
 		if (keystate[KEY_INPUT_DOWN])
 		{
-			if (data < 1.00f)
-			{
-				data += 0.01f;
-			}
+			data += 0.01f;
 		}
 		ClearDrawScreen();
 		// DrawGraph(0,0,image,true);
 		DrawGraph(0, 0, bg, true);
 		threshold[0] = data;
+		threshold[1] = angle;
 		DxLib::UpdateShaderConstantBuffer(cBuff);
 		SetShaderConstantBuffer(cBuff, DX_SHADERTYPE_PIXEL, 0);
-		ShadeGraph(10, 0, image, pt, ps);
+		ShadeDrawGraph(50, -20, image, pt, ps, normImg);
 
 		ScreenFlip();
 	}
